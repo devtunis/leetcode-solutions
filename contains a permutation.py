e9cd5ebdc761431b00a1ac3fa5b1cab9e77f2816
Gaith O’Nahdi <nahdigyth@gmail.com>
2026-03-24 05:20:25 +0100

    Add files via upload

---
+++ contains a permutation.py
@@ -0,0 +1,32 @@
+from collections import Counter
+
+class Solution(object):
+    def checkInclusion(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        len1, len2 = len(s1), len(s2)
+        if len1 > len2:
+            return False
+
+        count1 = Counter(s1)   
+        window = Counter(s2[:len1])   
+
+        if count1 == window: 
+            return True
+
+        
+        for i in range(len1, len2):
+          
+            window[s2[i]] += 1
+         
+            window[s2[i - len1]] -= 1
+            if window[s2[i - len1]] == 0:
+                del window[s2[i - len1]]
+ 
+            if window == count1:
+                return True
+
+        return False
